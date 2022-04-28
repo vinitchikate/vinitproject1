1dd1ac336f8e0dfc4030ec5f07df717f8c6534ed
@52567_vinit <vinitchikate123@gmail.com>
2022-04-28 09:16:59 +0530

    project

--- src/controllers/blogsController.js
+++ src/controllers/blogsController.js
@@ -3,20 +3,17 @@ const blogModel = require('../models/blogModel');
 const moment = require("moment");
 
 
-// Kirtan Gajjar
+
 const postBlogs = async function (req, res) {
     try {
-        // Get authorId in request body only.
-        if (!req.body.author) {
+        if (!req.body.authorId) {
             return res.status(400).send({ status: false, msg: "First Add Author-Id In Body" });
         }
-
         // Make sure the authorId is a valid authorId by checking the author exist in the authors collection.
-        let authorid = await authorModel.findById(req.body.author);
+        let authorid = await authorModel.findById(req.body.authorId);
         if (!authorid) {
             return res.status(400).send({ status: false, msg: "Plz Enter Valid Author Id" });
         }
-
         // Create a blog document from request body.
         let createblogs = await blogModel.create(req.body);
         // Return HTTP status 201 on a succesful blog creation. Also return the blog document. The response should be a JSON object
@@ -28,46 +25,52 @@ const postBlogs = async function (req, res) {
 };
 module.exports.postBlogs = postBlogs;
 
-// Amit Vishvakarma
+// ****************GET BLOGS API********************//
 const getBlogs = async function (req, res) {
     try {
-        let authorid = req.query.authorId;
-        let byCategory = req.query.category;
-        let tags = req.query.tags;
-        let subcategory = req.query.subcategory;
-        let allblogs = await blogModel.find({ isDeleted: false, isPublished: true, $or: [{ authorId: authorid }, { category: byCategory }, { tags: tags }, { subcategory: subcategory }] });
+        let data = req.query
+        //Returns all blogs in the collection that aren't deleted and are published
+        let blogs = await blogModel.find({ $and: [{ isDeleted: false }, { isPublished: true }, data] })
 
-        return res.status(200).send({ status: true, data: allblogs });
+        if (blogs.length <= 0) {
+            return res.status(400).send({ status: false, msg: 'data is not found' })
+        }
+
+        return res.status(200).send({ status: true, data: blogs })
     }
     catch (err) {
-        return res.status(500).send({ status: false, msg: err.message });
+        return res.status(500).send({ status: false, msg: err.message })
     }
 }
 module.exports.getBlogs = getBlogs
 
 
-// Salman Sayyed
+// ****************PUT API********************//
 const updateblogs = async function (req, res) {
     let data = req.body
     let blogId = req.params.blogId;
     let blog = await blogModel.findById(blogId);
     let dt = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
     if (blog) {
         if (blog.isDeleted == false) {
             if (blog.isPublished == true) {
                 let updatedate = await blogModel.findOneAndUpdate({ _id: blogId }, { new: true }, { $set: { publishedAt: dt } })
-                return res.status(200).send({ status: true, data: updatedate })
+
             }
-            let updateblogs = await blogModel.findOneAndUpdate({ _id: blogId }, { ...data }, { new: true })
-            return res.status(200).send({ status: true, data: updateblogs })
+
         }
+        let updateblogs = await blogModel.findOneAndUpdate({ _id: blogId }, { ...data }, { new: true })
+        return res.status(200).send({ status: true, data: updateblogs })
     }
 };
-module.exports.updateblogs = updateblogs
 
-// Kirtan Gajjar
+// *****************DeleteApi*************************//
+
 const deleteBlogs = async function (req, res) {
     try {
+
         let blogId = req.params.blogId;
         // Check if the blogId exists
         if (!blogId) {
@@ -96,4 +99,30 @@ const deleteBlogs = async function (req, res) {
 }
 module.exports.deleteBlogs = deleteBlogs;
 
-// Salman Sayyed
+// ********************DeleteByQuery***************************//
+
+const queryDeleted = async function (req, res) {
+    try {
+        let data = req.query;
+        console.log(data)
+        if (Object.values(data) <= 0)
+            return res.status(400).send({ status: false, msg: "Input Missing" });
+
+        let deleted = await blogModel.updateMany({ $and: [data, { isPublished: false }] }, { isDeleted: true, deleteAt: Date.now() }, { new: true });
+        console.log(deleted)
+        
+        if (deleted.modifiedCount == 0) {
+
+            return res.status(404).send({ status: false, msg: "Data cannot be modified" });
+        }
+        return res.status(200).send({ status: true, data: deleted });
+    }
+    catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+}
+
+
+module.exports.queryDeleted = queryDeleted
+
+module.exports.updateblogs = updateblogs
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,24 +2,32 @@ const express = require("express");
 const router = express.Router();
 const authorController = require("../controllers/authorController");
 const blogsController = require("../controllers/blogsController");
+const { validateAuthor,validateBlog } = require("../middleware/middleware");
+
 
 // Phase I
+// Author APIs /authors //Vinit
+router.post("/createauthor",validateAuthor, authorController.createauthor);
 
-// Author APIs /authors //Vinit Chikate
-router.post("/createauthor", authorController.createauthor);
 
-// POST /blogs //Kirtan Gajjar
-router.post("/blogs", blogsController.postBlogs);
+// POST /blogs //Kirtan
+router.post("/createBlogs",validateBlog, blogsController.postBlogs);
 
-// GET /blogs //Amit Vishvakarma
+
+// GET /blogs //Amit
 router.get("/getBlogs", blogsController.getBlogs);
 
-// PUT /blogs/:blogId //Salman Sayyed
+
+// PUT /blogs/:blogId //Salman
 router.put("/blogs/:blogId", blogsController.updateblogs);
 
-// DELETE /blogs/:blogId //Kirtan Gajjar
+
+// DELETE /blogs/:blogId //Kirtan
 router.delete("/blogs/:blogId", blogsController.deleteBlogs);
 
-// DELETE /blogs?queryParams //Salman Sayyed
+// DELETE /blogs?queryParams //Salman
+router.delete("/blogsq",blogsController.queryDeleted);
+
+
 
 module.exports = router;
